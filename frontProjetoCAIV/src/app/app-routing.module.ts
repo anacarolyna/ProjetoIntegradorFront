1f11911b017d24f4b07f8bed2c2539c779198123
RafaelBalbino <rafaelballabi@hotmail.com>
2022-06-09 16:32:04 -0300

    Atualizado em 09.06.2022 | @RafaelBalbino | Adc Fotos

--- frontProjetoCAIV/src/app/app-routing.module.ts
+++ frontProjetoCAIV/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'cadastro', component: CadastroComponent},
   {path: 'home', component: HomeComponent}
-
+  
 ];
 
 @NgModule({
