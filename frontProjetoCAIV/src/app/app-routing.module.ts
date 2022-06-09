6aa2a5e89d450e764c70304acc2e65c965e77ce8
unknown <taisanjostata@gmail.com>
2022-06-09 17:04:21 -0300

    Criação da navbar direita do inicio

--- frontProjetoCAIV/src/app/app-routing.module.ts
+++ frontProjetoCAIV/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
 
   {path:'login', component: LoginComponent},
   {path:'cadastro', component: CadastroComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'inicio', component: InicioComponent}
 
 ];
 
