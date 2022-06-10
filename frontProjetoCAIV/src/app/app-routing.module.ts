786f0bfc75ddc79b6e70d539144ae19d673eaf60
Benior <benior.santos@gmail.com>
2022-06-10 13:06:05 -0300

    criação da UI para editar e deletar o usuário

--- frontProjetoCAIV/src/app/app-routing.module.ts
+++ frontProjetoCAIV/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { UserDeleteComponent } from './delete/user-delete/user-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { HomeComponent } from './home/home.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +14,11 @@ const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'cadastro', component: CadastroComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+
+
+  {path: 'user-edit/:id', component: UserEditComponent},
+  {path: 'user-delete/:id', component: UserDeleteComponent}
 ];
 
 @NgModule({
